42a3efbb240f41f81c660749f643dcf53a5790a1
Akinola Akinpelu <akinpelu.akinola98@gmail.comccdxx   m,lA>
2022-11-11 07:03:35 +0100

    First

---
+++ src/Friends.js
@@ -0,0 +1,42 @@
+export const friends = [
+  {
+    name: "Fadedemi",
+    id: 1,
+    says: "I'm the only fadedemi that exists",
+  },
+  {
+    name: "Divine",
+    id: 2,
+    says: "Howfar na?",
+  },
+  {
+    name: "Akinwole",
+    id: 3,
+    says: "You get me yeah?",
+  },
+  {
+    name: "Purple",
+    id: 4,
+    says: "Akinola, oh my God",
+  },
+  {
+    name: "Ebere",
+    id: 5,
+    says: "You don't care about me",
+  },
+  {
+    name: "Samson",
+    id: 6,
+    says: "You dey show for south??",
+  },
+  {
+    name: "Kiitan",
+    id: 7,
+    says: "I'm an influencer!!!",
+  },
+  {
+    name: "Newness",
+    id: 8,
+    says: "I don't like you, Akin",
+  },
+];
---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import App from "./App";
+import Card from "./Card";
+import reportWebVitals from "./reportWebVitals";
+import { friends } from "./Friends";
+import "tachyons";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div className="Robots pa5">
+      <Card name={friends[0].name} says={friends[0].says} />
+      <Card name={friends[1].name} says={friends[1].says} />
+      <Card name={friends[2].name} says={friends[2].says} />
+      <Card name={friends[3].name} says={friends[3].says} />
+      <Card name={friends[4].name} says={friends[4].says} />
+      <Card name={friends[5].name} says={friends[5].says} />
+      <Card name={friends[6].name} says={friends[6].says} />
+      <Card name={friends[7].name} says={friends[7].says} />
+    </div>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
