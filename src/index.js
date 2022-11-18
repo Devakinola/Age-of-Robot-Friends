22583421b539149382758b3756a431db7c78a33f
Akinola Akinpelu <akinpelu.akinola98@gmail.comccdxx   m,lA>
2022-11-18 02:07:09 +0100

    yes boss

--- src/index.js
+++ src/index.js
@@ -1,27 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App";
-import Card from "./Card";
+import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
-import { friends } from "./Friends";
+
 import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <div className="Robots pa5">
-      <Card name={friends[0].name} says={friends[0].says} />
-      <Card name={friends[1].name} says={friends[1].says} />
-      <Card name={friends[2].name} says={friends[2].says} />
-      <Card name={friends[3].name} says={friends[3].says} />
-      <Card name={friends[4].name} says={friends[4].says} />
-      <Card name={friends[5].name} says={friends[5].says} />
-      <Card name={friends[6].name} says={friends[6].says} />
-      <Card name={friends[7].name} says={friends[7].says} />
-      <Card name={friends[8].name} says={friends[8].says} />
-      <Card name={friends[9].name} says={friends[9].says} />
-    </div>
+    <App />
   </React.StrictMode>
 );
 
