c627cd271defabe39130096eb511b96d5fd8e3e5
Akinola Akinpelu <akinpelu.akinola98@gmail.comccdxx   m,lA>
2022-12-18 08:18:12 +0100

    bina was here

--- src/Robots.js
+++ src/Robots.js
@@ -3,7 +3,12 @@ import Card from "./Card";
 export const RobotList = ({ friends }) => {
   const list = friends.map((user, i) => {
     return (
-      <Card key={friends[i].id} name={friends[i].name} says={friends[i].says} />
+      <Card
+        key={i}
+        id={friends[i].id}
+        name={friends[i].name}
+        says={friends[i].says}
+      />
     );
   });
   return <div>{list}</div>;
--- src/search.js
+++ src/search.js
@@ -1,10 +1,11 @@
-const Search = ({ searchvalue }) => {
+const Search = ({ searchvalue, searchchange }) => {
   return (
     <div className="pa2">
       <input
         className="pa3 ba b--grey hover-bg-moon-gray"
         type="search"
         placeholder={searchvalue}
+        onChange={searchchange}
       />
     </div>
   );
